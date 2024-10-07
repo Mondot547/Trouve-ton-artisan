ffcdad3dfeb46ce3148c216a4f163fc5a0b2ff27
Mondot547 <alex.mondot.mail.pro@gmail.com>
2024-10-07 17:27:58 +0200

    légére modification de style, mise en place du favicon et tentative de mise en ligne

--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -1,6 +1,6 @@
 import { Artisan } from './../../models/artisan.model';
 import { ActivatedRoute } from '@angular/router';
-
+import { Title, Meta } from '@angular/platform-browser';
 import { Component, OnInit } from '@angular/core';
 import { ArtisanCardComponent } from '../../component/artisan-card/artisan-card.component';
 import { CommonModule } from '@angular/common';
@@ -19,7 +19,9 @@ export class CategoryComponent implements OnInit {
 
   constructor(
     private artisanService: ArtisanSearchService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private titleService: Title,
+    private metaService: Meta
   ) {}
 
   // Récupérer la catégorie depuis les données de la route
@@ -27,6 +29,7 @@ export class CategoryComponent implements OnInit {
     this.route.data.subscribe((data) => {
       this.category = data['category'];
       this.loadArtisansByCategory(this.category);
+      this.setMetaData(this.category)
     });
   }
 
@@ -35,5 +38,13 @@ export class CategoryComponent implements OnInit {
     this.artisanService.getArtisansByCategory(category).subscribe((data) => {
       this.artisans = data;
     });
+  }
+
+  setMetaData(category: string | null) {
+    this.titleService.setTitle(`Catégorie: ${category}`);
+    this.metaService.updateTag({ name: 'description', content: `Découvrez les artisans dans la catégorie ${category}.` });
+    this.metaService.updateTag({ name: 'keywords', content: `artisan, ${category}, services` });
+    this.metaService.updateTag({ property: 'og:title', content: `Catégorie: ${category}` });
+    this.metaService.updateTag({ property: 'og:description', content: `Découvrez les artisans dans la catégorie ${category}.` });
   }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,6 +5,7 @@ import { Observable, of } from 'rxjs';
 import { Artisan } from '../../models/artisan.model';
 import { ArtisanSearchService } from '../../services/artisan.service';
 import { CommentSliderComponent } from '../../component/comment-slider/comment-slider.component';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -16,9 +17,22 @@ import { CommentSliderComponent } from '../../component/comment-slider/comment-s
 export class HomeComponent implements OnInit {
   topRatedArtisans$: Observable<Artisan[]> = of([]); // Liste des artisans les mieux notés
 
-  constructor(private artisanService: ArtisanSearchService) {}
+  constructor(
+    private artisanService: ArtisanSearchService,
+    private titleService: Title,
+    private metaService: Meta
+  ) {}
 
   ngOnInit(): void {
+    this.setMetaData();
     this.topRatedArtisans$ = this.artisanService.getTopRatedArtisans(); // Utilise le service pour récupérer les artisans
+  }
+
+  setMetaData() {
+    this.titleService.setTitle('Bienvenue sur notre site !');
+    this.metaService.updateTag({ name: 'description', content: 'Ceci est la page d\'accueil de notre site. Découvrez nos artisans.' });
+    this.metaService.updateTag({ name: 'keywords', content: 'artisan, accueil, services' });
+    this.metaService.updateTag({ property: 'og:title', content: 'Bienvenue sur notre site !' });
+    this.metaService.updateTag({ property: 'og:description', content: 'Ceci est la page d\'accueil de notre site. Découvrez nos artisans.' });
   }
 }
